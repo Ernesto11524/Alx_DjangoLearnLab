87dd2c6940f4e3408bef6a355625eb6fd69f4b05
Ernest Mpiani <ernestmpiani15@gmail.com>
2025-09-07 20:36:24 +0000

    No description

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib import messages
+
 from django.views.generic.detail import DetailView
 from .models import Library, Book
 
@@ -20,3 +24,35 @@ class LibraryDetailView(DetailView):
         # Add all related books
         context["books"] = self.object.books.all()
         return context
+    
+def register_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            messages.success(request, "Account created successfully! You can now log in.")
+            return redirect("login")
+    else:
+        form = UserCreationForm()
+    return render(request, "registration/register.html", {"form": form})
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, f"Welcome {username}!")
+                return redirect("/")   # redirect to home page
+        messages.error(request, "Invalid username or password")
+    else:
+        form = AuthenticationForm()
+    return render(request, "registration/login.html", {"form": form})
+
+def logout_view(request):
+    logout(request)
+    messages.info(request, "You have been logged out.")
+    return render(request, "registration/logout.html")
