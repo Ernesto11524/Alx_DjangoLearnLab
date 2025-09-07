07dc80a2307563260a35b1385927059b9243938c
Ernest Mpiani <ernestmpiani15@gmail.com>
2025-09-07 17:51:15 +0000

    No description

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,7 +1,8 @@
 from models import Author, Book, Library, Librarian
 
 author = Author.objects.create(name = "Ernest")
-All_books = Book.objects.all()
+library_name = Library.objects.create(name="Library_name")
+All_books = Library.objects.get(name=library_name)
 librarian1 = Librarian.objects.create(name = "George")
 library1 = librarian1.library
 
