111dc894ffe9db5e425c35a3f3242f62f91681fb
Ernest Mpiani <ernestmpiani15@gmail.com>
2025-09-15 15:32:24 +0000

    No description

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'relationship_app.apps.RelationshipAppConfig',
 ]
 
-AUTH_USER_MODEL = 'models.CustomUserModel'
+AUTH_USER_MODEL = 'bookshelf.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
