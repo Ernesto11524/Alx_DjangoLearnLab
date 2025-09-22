0ddfadd5b9c63f6b016dfbeb0e4ee15dc6aecfd8
Ernest Mpiani <ernestmpiani15@gmail.com>
2025-09-22 17:29:59 +0000

    No description

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -2,5 +2,7 @@ from rest_framework.test import APIRequestFactory
 from django.test import TestCase
 from .models import Book, Author
 from . import views
+from rest_framework import status
 
 # self, class, APITestCase
+# response.data
