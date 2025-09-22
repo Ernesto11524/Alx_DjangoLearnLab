f8b61f7cfb420cc391b6f7217ea2a75060f334af
Ernest Mpiani <ernestmpiani15@gmail.com>
2025-09-22 13:52:52 +0000

    No description

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('books/', views.ListView.as_view(), name='all-books'),
     path('books/<int:pk>/', views.DeleteView.as_view(),),
-    path('create/', views.CreateView.as_view(),),
-    path('update/', views.UpdateView.as_view(),),
-    path('delete/', views.DeleteView.as_view(),),
+    path('books/create/', views.CreateView.as_view(),),
+    path('books/update/', views.UpdateView.as_view(),),
+    path('books/delete/', views.DeleteView.as_view(),),
 ]
