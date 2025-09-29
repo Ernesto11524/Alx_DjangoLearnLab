e736600e3e9adc43560a535b78c80d4642ace5ec
Ernest Mpiani <ernestmpiani15@gmail.com>
2025-09-29 17:47:30 +0000

    No description

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -3,8 +3,12 @@ from .forms import CustomUserCreationForm, CustomUserChangeForm
 
 # Create your views here.
 def register(request):
-    form = CustomUserCreationForm()
-    return render(request, 'blog/registration.html', {'form' : form})
+    if request.method == "POST":
+        form = CustomUserCreationForm()
+        form.save()
+    else:
+        form = CustomUserCreationForm()
+        return render(request, 'blog/registration.html', {'form' : form})
 
 def profile(request):
     form = CustomUserChangeForm()
